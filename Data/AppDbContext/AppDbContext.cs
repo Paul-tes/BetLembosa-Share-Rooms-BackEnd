005a05df0291fc7d83489ac4054d5409a9e7cc15
paul-tes <paulasca8462@gmail.com>
2024-06-06 07:07:46 +0300

    Add essenetial packages and setup database connection with postgres

---
+++ Data/AppDbContext/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BetLembosa_Share_Rooms_BackEnd;
+
+public class AppDbContext : IdentityDbContext
+{
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+}
