a1c97b3838e509a0569d146606328f881c6a15eb
paul-tes <paulasca8462@gmail.com>
2024-06-07 11:20:39 +0300

    Add Home model class

---
+++ Dtos/Account/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BetLembosa_Share_Rooms_BackEnd;
+
+public class LoginDto
+{
+  [Required]
+  public string UserName { get; set; }
+
+  [Required]
+  public string Password { get; set; }
+}
