1daa599f488c0575f2375ecef855cbd35d6a80e2
paul-tes <paulasca8462@gmail.com>
2024-06-07 11:33:03 +0300

    Add Model classes in Dtos folleder completed

---
+++ Dtos/Trip/TripDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BetLembosa_Share_Rooms_BackEnd
+{
+    public class TripDto
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public Guid HomeId { get; set; }
+
+        // Navigation properties
+        public AppUser User { get; set; }
+        public HomeDto Home { get; set; }
+
+        // Additional trip information
+        public string TripInfo { get; set; }
+    }
+}
